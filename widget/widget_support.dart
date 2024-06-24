38661c3030324bb679df3c63d4855e5bf7ae9c13
SamsonGT <96789962+SamsonGulma@users.noreply.github.com>
2024-06-24 18:30:50 +0300

    Add files via upload

---
+++ widget/widget_support.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+class AppWidget {
+
+  static TextStyle boldTextFeildStyle() {
+    return
+     TextStyle(
+          color: Colors.black,
+          fontSize:20.0,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins');
+  }
+
+  static TextStyle HeadlineTextFeildStyle() {
+    return
+      TextStyle(
+          color: Colors.black,
+          fontSize:24.0,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins');
+  }
+
+  static TextStyle LightTextFeildStyle() {
+    return
+      TextStyle(
+          color: Colors.black38,
+          fontSize:17.0,
+          fontWeight: FontWeight.w500,
+          fontFamily: 'Poppins');
+  }
+
+  static TextStyle semiBoldTextFeildStyle() {
+    return
+      TextStyle(
+          color: Colors.black54,
+          fontSize:18.0,
+          fontWeight: FontWeight.w500,
+          fontFamily: 'Poppins');
+  }
+}
