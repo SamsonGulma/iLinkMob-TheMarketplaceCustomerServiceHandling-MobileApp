c9d998ec44a34cca314cd9623ab620d343949a6b
SamsonGT <96789962+SamsonGulma@users.noreply.github.com>
2024-06-27 01:48:18 +0300

    Update wishCart.dart

--- userPages/wishCart.dart
+++ userPages/wishCart.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class wishCart extends StatefulWidget {
-  const wishCart({super.key});
+import '../widget/widget_support.dart';
+
+class Cart extends StatefulWidget {
+  const Cart({super.key});
 
   @override
-  State<wishCart> createState() => _OrderState();
+  State<Cart> createState() => _CartState();
 }
 
-class _OrderState extends State<wishCart> {
+class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(7),
+        child: Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            padding: EdgeInsets.all(14),
+            height: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("images/dellLaptop.jpg", height: 150, width: 150, fit: BoxFit.cover,),
+                Text("Dell", style: AppWidget.semiBoldTextFeildStyle(),),
+                Text("Slim Dell touch screen", style: AppWidget.LightTextFeildStyle(),),
+                Text("ETB 58000", style: AppWidget.semiBoldTextFeildStyle(),),
+              ],),
+          ),
+        ),
+      ),
+
+    );
   }
 }
